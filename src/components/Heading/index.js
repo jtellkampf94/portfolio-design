aca3f7cd4d810a3bc272717695ee5658cf45f97b
Jonathan <jtellkampf@gmail.com>
2022-06-30 22:25:07 +0100

    restyled header

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -3,7 +3,17 @@ import "./index.scss";
 const Heading = () => {
   return (
     <div className="heading__background">
-      <h1 className="heading__title">Jonathan Tellkampf</h1>
+      <div className="heading__content">
+        <div className="heading__title-container">
+          <h1 className="heading__title-solid">Jonathan Tellkampf</h1>
+          <h1 className="heading__title-outline">Jonathan Tellkampf</h1>
+        </div>
+        <div className="heading__subtitle-container">
+          <h1 className="heading__subtitle-solid">Fullstack Developer</h1>
+          <h1 className="heading__subtitle-outline">Fullstack Developer</h1>
+        </div>
+      </div>
+      <div className="heading__box-1"></div>
     </div>
   );
 };
