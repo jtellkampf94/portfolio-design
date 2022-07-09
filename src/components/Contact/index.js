0b534f7676b252b90fba985bdfeffe4b6e8eb98c
Jonathan <jtellkampf@gmail.com>
2022-07-09 22:46:43 +0100

    Started contact secttion markup

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,64 @@
+import { useState } from "react";
+
+import "./index.scss";
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = () => {};
+
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section id="contact">
+      <form method="POST" className="contact__form" onSubmit={handleSubmit}>
+        <div className="contact__form-field">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="contact__form-field">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="contact__form-field">
+          <label htmlFor="subject">Subject</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+        </div>
+
+        <div className="contact__form-field">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target)}
+          ></textarea>
+        </div>
+
+        <button type="submit">Send Message</button>
+      </form>
+    </section>
   );
 };
 
