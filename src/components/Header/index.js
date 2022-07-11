7ad39914fe8ec43e8b7709c79e17d701fbfb9db6
Jonathan <jtellkampf@gmail.com>
2022-07-11 23:02:54 +0100

    added styles to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,21 @@
 import NavBar from "../NavBar";
 
+import "./index.scss";
+
 const Header = () => {
   return (
     <header id="header" className="header">
-      <NavBar />
+      <div className="header__image">
+        <NavBar />
 
-      <h1>Full Stack Web Developer & Web Designer</h1>
-      <h2>
-        Hi, I am Jonathan Tellkampf a full stack web developer and designer
-        based in North London. I love to create beautiful websites for the web.
-      </h2>
-      <button>Contact Me</button>
+        <h1>Full Stack Web Developer & Web Designer</h1>
+        <h2>
+          Hi, I am Jonathan Tellkampf a full stack web developer and designer
+          based in North London. I love to create beautiful websites for the
+          web.
+        </h2>
+        <button>Contact Me</button>
+      </div>
     </header>
   );
 };
