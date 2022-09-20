891fe4cb33003eeb9193d056132ed7a226022c96
Jonathan <jtellkampf@gmail.com>
2022-09-20 21:16:29 +0100

    Corrected styles in about section

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,48 +3,51 @@ import "./index.scss";
 const Skills = () => {
   return (
     <section className="skills" id="skills">
-      <h1>skills</h1>
-
-      <div className="skills__card">
-        <h4>Front End</h4>
-
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>SCSS</li>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-          <li>React</li>
-          <li>Redux</li>
-          <li>Apollo Client</li>
-          <li>NextJS</li>
-        </ul>
+      <div className="skills__heading">
+        <h1>skills</h1>
       </div>
+      <div className="skills__container">
+        <div className="skills__card glassomorphic">
+          <h4>Front End</h4>
 
-      <div className="skills__card">
-        <h4>Back End</h4>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SCSS</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Apollo Client</li>
+            <li>NextJS</li>
+          </ul>
+        </div>
 
-        <ul>
-          <li>NodeJS</li>
-          <li>ExpressJS</li>
-          <li>Apollo Server</li>
-          <li>AWS S3</li>
-          <li>MongoDB</li>
-          <li>PostgreSQL</li>
-          <li>Redux</li>
-        </ul>
-      </div>
+        <div className="skills__card glassomorphic">
+          <h4>Back End</h4>
 
-      <div className="skills__card">
-        <h4>Other</h4>
+          <ul>
+            <li>NodeJS</li>
+            <li>ExpressJS</li>
+            <li>Apollo Server</li>
+            <li>AWS S3</li>
+            <li>MongoDB</li>
+            <li>PostgreSQL</li>
+            <li>Redux</li>
+          </ul>
+        </div>
 
-        <ul>
-          <li>GIT</li>
-          <li>Docker</li>
-          <li>Docker-Compose</li>
-          <li>Linux</li>
-          <li>CMD / Powershell</li>
-        </ul>
+        <div className="skills__card glassomorphic">
+          <h4>Other</h4>
+
+          <ul>
+            <li>GIT</li>
+            <li>Docker</li>
+            <li>Docker-Compose</li>
+            <li>Linux</li>
+            <li>CMD / Powershell</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
