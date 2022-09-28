c2522ba0309812b5104a02178aa816ce39036f0e
Jonathan <jtellkampf@gmail.com>
2022-09-28 21:23:32 +0100

    Added typewriter to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,23 @@
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+
 import NavBar from "../NavBar";
 
 import "./index.scss";
 
 const Header = () => {
+  const [text, count] = useTypewriter({
+    words: ["Full Stack Web Developer", "Software Engineer"],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
     <header id="header" className="header">
       <div className="header__image">
         <div className="header__title ">
           <h5>Jonathan Tellkampf</h5>
-          <h1>Full Stack Web Developer</h1>
+          <h1>
+            {text} <Cursor cursorColor="#3195ff" />
+          </h1>
           <h2>I create beautiful and functional websites for the web.</h2>
           <a href="#contact">
             <button>Contact Me</button>
