73e4b5b055fa81337136f543973e3395b1afd03f
Jonathan <jtellkampf@gmail.com>
2022-10-19 14:52:42 +0100

    Made about and skills section responsive

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,12 +12,12 @@ const About = () => {
 
         <div className="about__description">
           <p>
-            Hi, I'm Jonathan Tellkampf, a full stack web developer based in the
-            North London area. I predominantly work with technologies that use
-            JavaScript, TypeScript and SQL programming languages. I am
-            passionate about coding and can work on both sides of the stack,
-            from building UI components on the front end, to implementing server
-            side functionality on the back end.
+            Hi, I'm Jonathan Tellkampf, a full stack web developer. I
+            predominantly work with technologies that use JavaScript, TypeScript
+            and SQL programming languages. I am passionate about coding and can
+            work on both sides of the stack, from building UI components on the
+            front end, to implementing server side functionality on the back
+            end.
           </p>
 
           <p>
